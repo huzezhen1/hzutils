15236c2d4d83ad7e24919bc38aba26b156be8475
huzezhen1 <395038209@qq.com>
2018-01-25 19:46:29 +0800

    修改karma配置

--- script/karma.conf.js
+++ script/karma.conf.js
@@ -97,6 +97,12 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
 
+    customLaunchers: {
+      Chrome: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
